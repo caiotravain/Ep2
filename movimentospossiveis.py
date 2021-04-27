e2b4c947a96084e77685a63328b826c1072087af
caio travain <caiotravain@gmail.com>
2021-04-27 19:06:26 -0300

    movimentos ainda nao pronto

---
+++ movimentospossiveis.py
@@ -0,0 +1,25 @@
+def extrai_naipe(x):
+    if '♥' in x:
+        return '♥'
+    if '♦' in x:
+        return '♦'
+    if '♣' in x:
+        return '♣'
+    if '♠' in x:
+        return '♠'
+def extrai_valor(x):
+    valor = x[:-1]
+    return valor
+
+def lista_movimentos_possiveis (baralho, indice):
+    movimentos = []
+    if indice == 0:
+        return movimentos
+    if extrai_valor(baralho[indice]) == extrai_valor(baralho[indice-1]):
+        movimentos.append(1)
+    elif extrai_naipe(baralho[indice]) == extrai_naipe(baralho[indice-1]):
+        movimentos.append(1)
+    if extrai_naipe(baralho[indice]) == extrai_naipe(baralho[indice-3]) and ((indice-3) >= 0) :
+        movimentos.append(3)
+    return movimentos
+print(lista_movimentos_possiveis(['A♦', 'J♥', 'Q♣', 'K♠', '10♣'],2))
